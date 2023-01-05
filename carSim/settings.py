86d914cb0c87c10d4d9081a013d99d8f8ecac279
Maximiliano Rosales <maxi.rosales202@gmail.com>
2023-01-05 21:41:58 +0100

    Changed temp file for saving meshes and ignore vscode folder

--- carSim/settings.py
+++ carSim/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import tempfile
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/tmp/'
+MEDIA_ROOT = os.path.join(tempfile.gettempdir(),'carSim/')
 
-MEDIA_URL = '/tmp/'
+MEDIA_URL = os.path.join(tempfile.gettempdir(),'carSim/')
